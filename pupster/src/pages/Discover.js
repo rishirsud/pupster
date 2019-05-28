f453dfa7ea41b8c41782037fed4af12e9341bed6
rishi <rishirsud@gmail.com>
2019-05-28 10:57:57 -0400

    Added filler text to discover page

--- pupster/src/pages/Discover.js
+++ pupster/src/pages/Discover.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Discover() {
-  return(<h1>Discover page</h1>);
+  return (
+    <React.Fragment>
+      <h1>Discover page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, nam aut. Nesciunt consectetur, maxime rem incidunt officiis odio obcaecati maiores, commodi culpa, temporibus vel et tenetur quis dolorum mollitia nulla! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur cupiditate quod nisi dolor dolores illum totam, dolorum corporis explicabo autem optio eligendi minima ipsam voluptatem commodi molestias, temporibus animi sint!</p>
+    </React.Fragment>
+  );
 }
 
 export default Discover;
